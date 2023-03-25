8cc5467588d49a5935a3349eb42ef02197cc14ab
Hong Linh <linhle.070801@gmail.com>
2023-03-25 10:37:26 +0700

    Cập nhật ui Alert Dialog

--- src/pages/UIAdvance/Dialog/index.tsx
+++ src/pages/UIAdvance/Dialog/index.tsx
@@ -2,7 +2,7 @@ import MainCard from '@components/Cards/MainCard';
 import SubCard from '@components/Cards/SubCard';
 import { Grid } from '@mui/material';
 import { gridSpacing } from '@shared/constant';
-import AlertDialog from '@views/uiAdvance/dialog/AlertDialog';
+import { AlertDialogView } from '@views/uiAdvance/dialog/AlertDialog';
 import React from 'react';
 
 function Dialog() {
@@ -11,7 +11,7 @@ function Dialog() {
       <Grid container spacing={gridSpacing}>
         <Grid item xs={4}>
           <SubCard title="Alert Dialog" contentSX={{ textAlign: 'center' }}>
-            <AlertDialog />
+            <AlertDialogView />
           </SubCard>
         </Grid>
       </Grid>
--- src/views/uiAdvance/dialog/AlertDialog.tsx
+++ src/views/uiAdvance/dialog/AlertDialog.tsx
@@ -8,7 +8,65 @@ import {
 } from '@mui/material';
 import React, { useCallback } from 'react';
 
-function AlertDialog() {
+import { type AlertDialogProps } from './type';
+
+function AlertDialog({
+  // main
+  open,
+  dialogProps,
+  // title
+  title = "Use Google's location service?",
+  titleProps,
+  // content
+  contentProps,
+  descProps,
+  description = 'Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.',
+  // actions
+  actionsProps,
+  customActions = false,
+  actionsContent,
+  btnLeftText,
+  btnLeftProps,
+  btnRightText,
+  btnRightProps,
+  // func
+  onClose,
+}: AlertDialogProps) {
+  return (
+    <Dialog {...dialogProps} open={open} onClose={onClose}>
+      <DialogTitle {...titleProps}>{title}</DialogTitle>
+      <DialogContent {...contentProps}>
+        <DialogContentText {...descProps}>{description}</DialogContentText>
+      </DialogContent>
+      <DialogActions {...actionsProps}>
+        {!customActions ? (
+          <>
+            <Button
+              {...btnLeftProps}
+              onClick={(e) => {
+                onClose();
+                if (btnLeftProps?.onClick !== undefined) {
+                  btnLeftProps.onClick(e);
+                }
+              }}
+            >
+              {btnLeftText ?? 'Hủy'}
+            </Button>
+            <Button variant="contained" {...btnRightProps}>
+              {btnRightText ?? 'Đồng ý'}
+            </Button>
+          </>
+        ) : (
+          actionsContent
+        )}
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default AlertDialog;
+
+export const AlertDialogView = () => {
   const [open, setOpen] = React.useState(false);
 
   const toggleDialog = useCallback((status?: boolean) => {
@@ -25,43 +83,17 @@ function AlertDialog() {
       >
         Open Alert Dialog
       </Button>
-      <Dialog
+      <AlertDialog
         open={open}
+        btnRightProps={{
+          onClick() {
+            toggleDialog();
+          },
+        }}
         onClose={() => {
           toggleDialog();
         }}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={() => {
-              toggleDialog();
-            }}
-          >
-            Disagree
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => {
-              toggleDialog();
-            }}
-          >
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
+      />
     </>
   );
-}
-
-export default AlertDialog;
+};
