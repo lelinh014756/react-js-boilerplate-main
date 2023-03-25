6944e185aaf3cc0a42a6b070b29f38b625dd4fbd
Hong Linh <linhle.070801@gmail.com>
2023-03-25 18:35:59 +0700

    Cập nhật ui alert not icon + alert basic

--- src/pages/UIAdvance/Alert/index.tsx
+++ src/pages/UIAdvance/Alert/index.tsx
@@ -3,11 +3,23 @@ import SubCard from '@components/Cards/SubCard';
 import { Grid } from '@mui/material';
 import { gridSpacing } from '@shared/constant';
 import AlertActions from '@views/uiAdvance/alert/AlertActions';
+import AlertBasic from '@views/uiAdvance/alert/AlertBasic';
+import AlertNotIcon from '@views/uiAdvance/alert/AlertNotIcon';
 
 function Alert() {
   return (
     <MainCard title="Dialog">
       <Grid container spacing={gridSpacing}>
+        <Grid item xs={6}>
+          <SubCard title="Alert basic" contentSX={{ textAlign: 'center' }}>
+            <AlertBasic />
+          </SubCard>
+        </Grid>
+        <Grid item xs={6}>
+          <SubCard title="Alert not icon" contentSX={{ textAlign: 'center' }}>
+            <AlertNotIcon />
+          </SubCard>
+        </Grid>
         <Grid item xs={6}>
           <SubCard
             title="Alert with actions"
