ff9c7a70227b0419121202a7e370ceb8fce34a81
Hong Linh <linhle.070801@gmail.com>
2023-03-29 22:13:17 +0700

    Thêm giao diện uploader preview wall box

--- src/pages/UIAdvance/Uploader/index.tsx
+++ src/pages/UIAdvance/Uploader/index.tsx
@@ -2,7 +2,8 @@ import MainCard from '@components/Cards/MainCard';
 import SubCard from '@components/Cards/SubCard';
 import { Grid } from '@mui/material';
 import { gridSpacing } from '@shared/constant';
-import UploaderPreview from '@views/uploader/UploadePreview';
+import UploaderPreview from '@views/uiAdvance/uploader/UploaderPreview';
+import UploaderWallView from '@views/uiAdvance/uploader/UploaderWall/UploaderWallView';
 import React from 'react';
 
 function Uploader() {
@@ -10,8 +11,13 @@ function Uploader() {
     <MainCard title="Uploader">
       <Grid container spacing={gridSpacing}>
         <Grid item xs={6}>
-          <SubCard title="Upload Preview">
+          <SubCard title="Uploader basic & preview" className="h-full">
             <UploaderPreview />
+          </SubCard>
+        </Grid>
+        <Grid item xs={6}>
+          <SubCard title="Uploader wall & preview" className="h-full">
+            <UploaderWallView />
           </SubCard>
         </Grid>
       </Grid>
